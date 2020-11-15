2f7bb25feef0696800730bd30f8a0dc5c2da785a
Oleg Freedom <oleg_freedom@rambler.ru>
2020-11-15 21:28:08 +0200

    Test

--- Models/Queries.php
+++ Models/Queries.php
@@ -48,7 +48,7 @@ class Queries {
         /** @var \PDOStatement $pdoStatement */
         $sth = DB::getPdo()->prepare($sql);
         $sth->execute($params);
-        $cronIds = $sth->fetchColumn('id');
+        $cronIds = $sth->fetchColumn();
 
         return $cronIds;
     }
