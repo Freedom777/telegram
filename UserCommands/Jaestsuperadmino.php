1ab9df82f970d3502b4b5ed3186fdd3a71811419
Oleg Freedom <oleg_freedom@rambler.ru>
2020-12-06 21:41:58 +0200

    Debug

--- UserCommands/Jaestsuperadmino.php
+++ UserCommands/Jaestsuperadmino.php
@@ -2,6 +2,7 @@
 
 namespace Longman\TelegramBot\Commands\UserCommands;
 
+use Longman\TelegramBot\TelegramLog;
 use Models\BasePdo;
 use Models\Logic;
 use Models\UserCommand;
@@ -50,12 +51,13 @@ class JaestsuperadminoCommand extends UserCommand
             'filters' => ['chat_id' => $this->user_id],
             'limit' => 1
         ]);
+        TelegramLog::error(var_export($amocrmUsers, true));
         if (!empty($amocrmUsers)) {
             $id = $amocrmUsers [0] ['id'];
 
             $result = Logic::updateAmocrmUser([
                 'amocrm_user_type' => self::$AMOCRRM_USER_TYPE_ADMIN,
-                'updated_at' => BasePdo::now()
+                'updated_at' => BasePdo::now(),
             ], [
                 'id' => $id
             ]);
