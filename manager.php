0a31c51cd0b593012b2933781752346e1ea71a1a
Oleg Freedom <oleg_freedom@rambler.ru>
2020-12-06 12:24:38 +0200

    Added admins

--- manager.php
+++ manager.php
@@ -10,7 +10,6 @@ use Longman\TelegramBot\Entities\Update;
 use Longman\TelegramBot\Request;
 use Longman\TelegramBot\TelegramLog;
 use Models\BasePdo;
-use Models\Logic;
 
 require_once __DIR__ . DIRECTORY_SEPARATOR . 'settings.php';
 
@@ -18,9 +17,9 @@ require_once __DIR__ . DIRECTORY_SEPARATOR . 'settings.php';
 try {
     $dbh = new PDO('mysql:host=' . $botSettings['mysql']['host'] . ';dbname=' . $botSettings['mysql']['database'],
         $botSettings['mysql']['user'], $botSettings['mysql']['password']);
-    $admins = array_column(Logic::getAmocrmUsers([
+    $admins = array_column(BasePdo::select('amocrm_user', [
         'fields' => 'chat_id',
-        'filters' => ['amocrm_user_type' => 'admin']
+        'where' => ['amocrm_user_type' => 'admin']
     ]), 'chat_id');
     $dbh = null;
     array_walk($admins, function (&$item) {
